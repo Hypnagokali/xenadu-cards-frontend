bf908caf974675246f8932d625fe3035da96d184
Stefan Simon <stefan.simon@xenadu.de>
2022-11-20 13:42:05 +0100

    Updated CardSet

--- src/pages/manage-cards/ManageCardSets.vue
+++ src/pages/manage-cards/ManageCardSets.vue
@@ -14,37 +14,39 @@
     </div>
     <div class="row">
       <div class="col-md-12">
-      <q-table
-        :grid="$q.screen.lt.sm"
-        title="Card-Sets"
-        :columns="columns"
-        :rows="cardSets"
-        row-key="id"
-      >
-        <template v-slot:body-cell-actions="props">
-          <q-td>
-            <q-btn
-              class="float-right"
-              flat
-              icon="delete"
-              @click="confirmDeleteCardSet(props.row)"
-            />
-            <q-btn
-              class="float-right"
-              flat
-              icon="edit"
-              @click="confirmAddOrUpdateCardSet(cardSetActions.UPDATE, props.row)"
-            />
-            <q-btn
-              class="float-right"
-              flat
-              icon="visibility"
-              @click="showCards(props.row)"
-            />
-          </q-td>
-        </template>
-        <template v-slot:item="props">
-          <div class="q-mb-md" style="width: 90vw">
+        <q-table
+          :grid="$q.screen.lt.sm"
+          title="Card-Sets"
+          :columns="columns"
+          :rows="cardSets"
+          row-key="id"
+        >
+          <template v-slot:body-cell-actions="props">
+            <q-td>
+              <q-btn
+                class="float-right"
+                flat
+                icon="delete"
+                @click="confirmDeleteCardSet(props.row)"
+              />
+              <q-btn
+                class="float-right"
+                flat
+                icon="edit"
+                @click="
+                  confirmAddOrUpdateCardSet(cardSetActions.UPDATE, props.row)
+                "
+              />
+              <q-btn
+                class="float-right"
+                flat
+                icon="visibility"
+                @click="showCards(props.row)"
+              />
+            </q-td>
+          </template>
+          <template v-slot:item="props">
+            <div class="q-mb-md" style="width: 90vw">
               <q-card>
                 <div class="row">
                   <div class="col-8" @click="showCards(props.row)">
@@ -61,7 +63,12 @@
                         class="float-right"
                         flat
                         icon="edit"
-                        @click="editCardSet(props.row)"
+                        @click="
+                          confirmAddOrUpdateCardSet(
+                            cardSetActions.UPDATE,
+                            props.row
+                          )
+                        "
                       />
                     </q-card-section>
                   </div>
@@ -76,25 +83,35 @@
                     </q-card-section>
                   </div>
                 </div>
-
               </q-card>
-          </div>
-        </template>
-      </q-table>
-    </div>
+            </div>
+          </template>
+        </q-table>
+      </div>
     </div>
     <q-dialog v-model="confirmDelete" persistent>
       <q-card>
         <q-card-section class="row items-center">
           <q-avatar icon="report_problem" color="primary" text-color="white" />
-          <span class="q-ml-sm">Do you want to delete the CardSet <strong>{{ selectedCardSet.name }}</strong>?</span>
+          <span class="q-ml-sm"
+            >Do you want to delete the CardSet
+            <strong>{{ selectedCardSet.name }}</strong
+            >?</span
+          >
           <br />
-          <p class="q-ml-sm">Warning: all cards will be lost after action "delete"!</p>
+          <p class="q-ml-sm">
+            Warning: all cards will be lost after action "delete"!
+          </p>
         </q-card-section>
 
         <q-card-actions align="right">
           <q-btn flat label="Cancel" color="secondary" v-close-popup />
-          <q-btn flat label="Delete" color="negative" @click="deleteSelectedCardSet()" />
+          <q-btn
+            flat
+            label="Delete"
+            color="negative"
+            @click="deleteSelectedCardSet()"
+          />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -105,16 +122,41 @@
           <div class="text-h6">Name of card set</div>
         </q-card-section>
 
-        <q-card-section class="q-pt-none" v-if="cardSetAction === cardSetActions.ADD">
-          <q-input dense v-model="newCardSet.name" autofocus @keyup.enter="addNewCardSet()" />
+        <q-card-section
+          class="q-pt-none"
+          v-if="cardSetAction === cardSetActions.ADD"
+        >
+          <q-input
+            dense
+            v-model="newCardSet.name"
+            autofocus
+            @keyup.enter="addNewCardSet()"
+          />
         </q-card-section>
         <q-card-section class="q-pt-none" v-else>
-          <q-input dense v-model="selectedCardSet.name" autofocus @keyup.enter="updateCardSet()" />
+          <q-input
+            dense
+            v-model="selectedCardSet.name"
+            autofocus
+            @keyup.enter="updateCardSet()"
+          />
         </q-card-section>
 
         <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Cancel" v-close-popup @click="resetNewCardSet()"/>
-          <q-btn flat label="Add CardSet" @click="addNewCardSet()" />
+          <q-btn flat label="Cancel" v-close-popup @click="resetNewCardSet()" />
+
+          <q-btn
+            v-if="cardSetAction === cardSetActions.ADD"
+            flat
+            label="Add CardSet"
+            @click="addNewCardSet()"
+          />
+          <q-btn
+            v-if="cardSetAction === cardSetActions.UPDATE"
+            flat
+            label="Update CardSet"
+            @click="updateCardSet()"
+          />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -128,6 +170,7 @@ import { useRouter } from "vue-router";
 import { api } from "boot/axios";
 import { useUserStore } from "stores/userStore";
 
+// Refs and constants
 const columns = [
   {
     name: "id",
@@ -169,19 +212,19 @@ const cardSets = ref([
 const cardSetAction = ref("ADD");
 const cardSetActions = {
   ADD: "ADD",
-  UPDATE: "Update"
+  UPDATE: "Update",
 };
 
 const confirmDelete = ref(false);
 
 const selectedCardSet = ref({
-  name: 'None',
+  name: "None",
 });
 
 const newCardSet = ref({
   id: 0,
   userId: 0,
-  name: '',
+  name: "",
 });
 
 const addOrUpdateCardSetModal = ref(false);
@@ -190,11 +233,15 @@ const user = ref({
   id: -1,
 });
 
+// functions
+
 const handleNew = (user) => {
+  console.log("handleNew");
+  console.log("user", user.value);
   newCardSet.value = {
-    id: user.id,
-    userId: 0,
-    name: '',
+    id: 0,
+    userId: user.value.id,
+    name: "",
   };
 };
 
@@ -204,20 +251,24 @@ const handleUpdate = (currentCardSet) => {
   }
 
   selectedCardSet.value = currentCardSet;
+  console.log("selectedCardSet was set to", selectedCardSet.value);
 };
 
-const handleUserAndCardErrors = () => {
-  if (newCardSet.value.userId === 0) {
-    XenaduNotify.error('Benutzer konnte nicht gelesen werden.');
-    return;
+const isUserNotSetOrMessageEmpty = () => {
+  const cardSetReference =
+    cardSetAction.value === cardSetActions.ADD ? newCardSet : selectedCardSet;
+  if (cardSetReference.value.userId === 0) {
+    XenaduNotify.error("Benutzer konnte nicht gelesen werden.");
+    return true;
   }
-  // Do axios call
-  if (newCardSet.value.name.trim().length === 0) {
-    XenaduNotify.warning("CardSet without name cannot be added");
-    return;
-  }
-}
 
+  if (cardSetReference.value.name.trim().length === 0) {
+    XenaduNotify.warning("CardSet without name cannot be added");
+    return true;
+  }
+
+  return false;
+};
 
 export default defineComponent({
   name: "ManageCardSets",
@@ -226,14 +277,15 @@ export default defineComponent({
     const userStore = useUserStore();
 
     onMounted(() => {
-      userStore.getCurrentOrFetchUser()
-      .then(fetchedUser => {
-        user.value = fetchedUser;
-      })
-      .catch(() => {
-        console.error('Fehler beim Laden des Users');
-      });
-      console.log('Lese User: ', user.value);
+      userStore
+        .getCurrentOrFetchUser()
+        .then((fetchedUser) => {
+          user.value = fetchedUser;
+          console.log("Lese User: ", user.value);
+        })
+        .catch(() => {
+          console.error("Fehler beim Laden des Users");
+        });
     });
 
     const router = useRouter();
@@ -242,12 +294,13 @@ export default defineComponent({
       router.push(`/manage-card-sets/${cardSet.id}`);
     };
 
-    api.get('/api/card-sets')
-    .then(res => {
-      console.log(res);
-      cardSets.value = res.data;
-    }).catch(err => console.log(err));
-
+    api
+      .get("/api/card-sets")
+      .then((res) => {
+        console.log(res);
+        cardSets.value = res.data;
+      })
+      .catch((err) => console.log(err));
 
     return {
       confirmDelete,
@@ -259,8 +312,7 @@ export default defineComponent({
       newCardSet,
       addOrUpdateCardSetModal,
       confirmAddOrUpdateCardSet(action, selectedCardSet = null) {
-        addOrUpdateCardSetModal.value = true;
-        cardSetAction.value = action;
+        console.log(selectedCardSet);
 
         if (action === cardSetActions.UPDATE) {
           handleUpdate(selectedCardSet);
@@ -268,51 +320,82 @@ export default defineComponent({
           handleNew(user);
         }
 
+        addOrUpdateCardSetModal.value = true;
+        cardSetAction.value = action;
       },
       resetNewCardSet() {
-        newCardSet.value.name = '';
+        newCardSet.value.name = "";
       },
       confirmDeleteCardSet(cardSet) {
         selectedCardSet.value = cardSet;
         confirmDelete.value = true;
       },
       deleteSelectedCardSet() {
-        api.delete(`/api/card-sets/${selectedCardSet.value.id}`)
-        .then(() => {
-          XenaduNotify.info(`CardSet '${selectedCardSet.value.name}' deleted`);
-          const index = cardSets.value.findIndex(c => c.id === selectedCardSet.value.id);
-          if (index > -1) {
-            cardSets.value.splice(index, 1);
-          }
-        })
-        .catch((e) => {
-          console.log(e);
-          XenaduNotify.error('Action "delete" could not be executed');
-        }).finally(() => {
-          confirmDelete.value = false;
-        });
+        api
+          .delete(`/api/card-sets/${selectedCardSet.value.id}`)
+          .then(() => {
+            XenaduNotify.info(
+              `CardSet '${selectedCardSet.value.name}' deleted`
+            );
+            const index = cardSets.value.findIndex(
+              (c) => c.id === selectedCardSet.value.id
+            );
+            if (index > -1) {
+              cardSets.value.splice(index, 1);
+            }
+          })
+          .catch((e) => {
+            console.log(e);
+            XenaduNotify.error('Action "delete" could not be executed');
+          })
+          .finally(() => {
+            confirmDelete.value = false;
+          });
       },
       editCardSet() {
         addOrUpdateCardSetModal.value = true;
-        console.log('do edit');
+        console.log("do edit");
       },
       addNewCardSet() {
-        handleUserAndCardErrors();
+        if (isUserNotSetOrMessageEmpty()) return;
 
-        api.post('/api/card-sets', newCardSet.value)
-          .then(res => {
+        api
+          .post("/api/card-sets", newCardSet.value)
+          .then((res) => {
             cardSets.value.push(res.data);
           })
-          .catch(err => {
+          .catch((err) => {
             console.log(err);
-            XenaduNotify.error('Card set could not be added');
+            XenaduNotify.error("Card set could not be added");
           });
 
-        this.addOrUpdateCardSetModal = false;
+        addOrUpdateCardSetModal.value = false;
         this.resetNewCardSet();
       },
       updateCardSet() {
+        console.log("Update CardSet");
+        if (isUserNotSetOrMessageEmpty()) return;
 
+        api
+          .put(
+            `/api/card-sets/${selectedCardSet.value.id}`,
+            selectedCardSet.value
+          )
+          .then((res) => {
+            const updatedCardSet = res.data;
+            const index = cardSets.value.findIndex(
+              (c) => c.id === selectedCardSet.value.id
+            );
+            if (index > -1) {
+              cardSets.value.splice(index, 1, updatedCardSet);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            XenaduNotify.error("Card set could not be added");
+          });
+        addOrUpdateCardSetModal.value = false;
+        this.selectedCardSet.value = null;
       },
       showCards,
     };
