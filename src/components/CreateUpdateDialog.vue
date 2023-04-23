ca777d943c97694acdf765e09b2f9ba63472c565
Stefan Simon <stefan.j.c.simon@gmail.com>
2023-04-23 16:14:49 +0200

    [+] CreateUpdateDialog as component

---
+++ src/components/CreateUpdateDialog.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-dialog v-model="isShow" persistent>
+    <q-card style="min-width: 350px">
+      <q-card-section>
+        <div class="text-h6">{{ resource }}</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <!-- SLOT: input elements-->
+        <slot name="inputElements"></slot>
+      </q-card-section>
+      <q-card-actions align="right" class="text-primary">
+        <q-btn flat label="Cancel" @click="reset()" />
+
+        <q-btn
+          v-if="resourceAction === actions.CREATE"
+          flat
+          :label="`Add ${resource}`"
+          @click="create()"
+        />
+        <q-btn
+          v-if="resourceAction === actions.UPDATE"
+          flat
+          :label="`Update ${resource}`"
+          @click="update()"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { defineProps, ref, computed } from 'vue';
+import { XenaduNotify } from 'src/composables/xenadu-notify';
+
+const props = defineProps({
+  resource: String,
+  id: Number,
+  show: Boolean,
+});
+
+const isShow = computed(() => {
+  return props.show;
+});
+
+const emit = defineEmits(['reset', 'create', 'update']);
+
+const actions = {
+  CREATE: 'create',
+  UPDATE: 'update',
+};
+
+let resourceAction = actions.CREATE;
+
+if (props.id == null) {
+  XenaduNotify.error('Something is wrong with the resource');
+} else if (props.id > 0) {
+  resourceAction = actions.UPDATE;
+}
+
+const reset = function () {
+  emit('reset');
+};
+
+const create = function () {
+  emit('create');
+};
+
+const update = function () {
+  emit('update');
+};
+
+const getAction = function () {
+  if (actions.CREATE) {
+    return create;
+  } else {
+    return update;
+  }
+};
+</script>
--- src/pages/manage-cards/ManageLessons.vue
+++ src/pages/manage-cards/ManageLessons.vue
@@ -2,6 +2,15 @@
   <div>
     <h3>Manage Lessons</h3>
 
+    <div class="row q-mb-sm q-mr-sm">
+      <div class="col-12">
+        <q-btn
+          class="float-right"
+          label="New Lesson"
+          @click="showCreateUpdateDialog = true"
+        />
+      </div>
+    </div>
     <div class="row">
       <div class="col-md-12">
         <q-table
@@ -11,21 +20,43 @@
           :rows="lessons"
           row-key="id"
         >
-          <template>
+          <template v-slot:body-cell-actions>
             <q-td>
               <q-btn class="float-right" flat icon="delete" />
+              <q-btn class="float-right" flat icon="edit" />
+              <q-btn class="float-right" flat icon="source" />
             </q-td>
           </template>
         </q-table>
       </div>
     </div>
+    <create-update-dialog
+      @reset="reset()"
+      resource="Lesson"
+      :id="0"
+      :show="showCreateUpdateDialog"
+    >
+      <template v-slot:inputElements>
+        <q-input label="Lesson Name" v-model="selectedLesson.name" />
+      </template>
+    </create-update-dialog>
   </div>
 </template>
 
 <script>
+import { api } from 'src/boot/api';
+import CreateUpdateDialog from 'src/components/CreateUpdateDialog.vue';
+import { useRoute } from 'vue-router';
 import { ref } from 'vue';
+import { XenaduNotify } from 'src/composables/xenadu-notify';
 
 const columns = [
+  {
+    name: 'id',
+    field: 'id',
+    label: 'ID',
+    sortable: true,
+  },
   {
     name: 'name',
     field: 'name',
@@ -39,29 +70,57 @@ const columns = [
     sortable: true,
   },
   {
-    name: 'action',
+    name: 'actions',
     label: 'Action',
   },
 ];
 
 const lessons = ref([
   {
+    id: 23,
     name: 'Lesson 1',
     cards: 50,
   },
   {
+    id: 89,
     name: 'Lessons 2',
     cards: 4,
   },
 ]);
 
+const selectedLesson = ref({
+  id: 0,
+  name: '',
+});
+
 export default {
   name: 'ManageLessons',
+  components: {
+    CreateUpdateDialog,
+  },
 
   setup() {
+    const route = useRoute();
+    const cardSetId = route.params.cardSetId;
+
+    const showCreateUpdateDialog = ref(false);
+    api
+      .get(`/api/card-sets/${cardSetId}/lessons`)
+      .then((res) => {
+        lessons.value = res.data;
+      })
+      .catch((e) => {
+        XenaduNotify.error('could not retrieve lessons');
+      });
+
     return {
       columns,
       lessons,
+      selectedLesson,
+      showCreateUpdateDialog,
+      reset() {
+        showCreateUpdateDialog.value = false;
+      },
     };
   },
 };
